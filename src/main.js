a6d4adbeafdcc9ccca7027769416ad15a42f66f3
cucu <2330256454@qq.com>
2026-05-26 00:30:56 +0800

    restore page

--- src/main.js
+++ src/main.js
@@ -1367,8 +1367,6 @@ function loadModel() {
   loader.load(
     "/models/scene.gltf",
     (gltf) => {
-       console.log("模型成功加载");
-    console.log(gltf);
       const model = gltf.scene;
       const box = new THREE.Box3().setFromObject(model);
       const size = box.getSize(new THREE.Vector3());
